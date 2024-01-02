7931c5a25c2769251a99c5147dd3d518f6169aef
Matt Oklahuman <theoklahuman@gmail.com>
2024-01-02 08:35:32 -0600

    Work on userinput and result

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./Header"
+import Result from "./Result"
 import UserInput from "./UserInput"
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div>
       <Header />
       <UserInput />
+      <Result />
     </div>
   )
 }
--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -1,32 +1,77 @@
+import { useState } from "react";
+
 function UserInput() {
+  const [currentInvestment, setCurrentInvestment] = useState({
+    initialInvestment: "",
+    annualInvestment: "",
+    expectedReturn: "",
+    duration: "",
+  });
+
+  function handleInitialInvestmentChange(evt) {
+    setCurrentInvestment({
+      ...currentInvestment,
+      initialInvestment: evt.target.value,
+    });
+  }
+  function handleAnnualInvestmentChange(evt) {
+    setCurrentInvestment({
+      ...currentInvestment,
+      annualInvestment: evt.target.value,
+    });
+  }
+  function handleExpectedReturnChange(evt) {
+    setCurrentInvestment({
+      ...currentInvestment,
+      expectedReturn: evt.target.value,
+    });
+  }
+  function handleDurationChange(evt) {
+    setCurrentInvestment({
+      ...currentInvestment,
+      duration: evt.target.value,
+    });
+  }
   return (
     <div id="user-input">
       <div className="input-group">
         <div>
-            <label htmlFor="">
-              Initial Investment
-            </label>
-            <input type="number" />
+          <label htmlFor="initialInvestment">Initial Investment</label>
+          <input
+            type="number"
+            name="initialInvestment"
+            value={currentInvestment.initialInvestment}
+            onChange={handleInitialInvestmentChange}
+          />
         </div>
         <div>
-            <label htmlFor="">
-              Annual Investment
-            </label>
-            <input type="number" />
+          <label htmlFor="annualInvestment">Annual Investment</label>
+          <input
+            type="number"
+            name="annualInvestment"
+            value={currentInvestment.annualInvestment}
+            onChange={handleAnnualInvestmentChange}
+          />
         </div>
       </div>
       <div className="input-group">
         <div>
-            <label htmlFor="">
-              Expected Return
-            </label>
-            <input type="number" />
+          <label htmlFor="expectedReturn">Expected Return</label>
+          <input
+            type="number"
+            name="expectedReturn"
+            value={currentInvestment.expectedReturn}
+            onChange={handleExpectedReturnChange}
+          />
         </div>
         <div>
-            <label htmlFor="">
-              Duration
-            </label>
-            <input type="number" />
+          <label htmlFor="duration">Duration</label>
+          <input
+            type="number"
+            name="duration"
+            value={currentInvestment.duration}
+            onChange={handleDurationChange}
+          />
         </div>
       </div>
     </div>
